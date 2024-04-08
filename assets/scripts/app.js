cbf8df954b3290f31f62f8b9e7cc9cc94cc9f8e8
Jaipal Ramidi <jaipal.ramidi@ONPASSIVE.ORG>
2024-04-08 18:14:02 +0530

    section-18

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,15 @@
+const listElement = document.querySelector(".posts");
+const postTemplate = document.getElementById("single-post")
 const xhr = new XMLHttpRequest();
 xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
+xhr.responseType = "json";
 xhr.onload = function () {
-    console.log(xhr.response);
+    const listOfPosts = xhr.response;
+    for (const post of listOfPosts) {
+        const listEl = document.importNode(postTemplate.content, true);
+        listEl.querySelector("h2").textContent = post.title.toUpperCase();
+        listEl.querySelector("p").textContent = post.body;
+        listElement.appendChild(listEl);
+    }
 }
 xhr.send();
